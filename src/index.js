62c5675e3d19b9979183f8e15b44f87a13a93c1f
Olga Verbetska <verbetska150@gmail.com>
2021-02-16 11:26:49 +0200

    create if

--- src/index.js
+++ src/index.js
@@ -9,10 +9,33 @@ const refs = {
 
 //debounced input
 
-refs.input.addEventListener('input', debounce (event => {
-        event.preventDefault();
-        console.log(event.target.value);
-      }, 500))
+refs.input.addEventListener(
+  'input',
+  debounce(event => {
+    event.preventDefault();
+    console.log(event.target.value);
+  }, 500),
+);
 
+//fetch
 
+const searchQuery = 'latvia';
+fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
+  .then(response => response.json())
+  .then(data => Console(data));
 
+function Console(data) {
+  console.log(data);
+  console.log(data.length);
+  if (data.length === 1) {
+      console.log(1);
+      return
+  }
+  if (data.length > 10) {
+      console.log('many');
+      return
+  }
+  else {
+      console.log('list')
+  }
+}
