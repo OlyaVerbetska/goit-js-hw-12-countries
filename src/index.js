534b06033dcd426fd37027bd687b7d6c36fb412a
Olga Verbetska <verbetska150@gmail.com>
2021-02-16 10:30:44 +0200

    create input debounce

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './styles.css';
+import debounce from 'lodash.debounce';
+
+//refs
+
+const refs = {
+  input: document.querySelector('#countries'),
+};
+
+//debounced input
+
+refs.input.addEventListener('input', debounce (event => {
+        event.preventDefault();
+        console.log(event.target.value);
+      }, 500))
+
+
+
